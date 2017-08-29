b22155533198103ec01a53e9cde92c1ad775b18e
Can Canbolat <can.canbolat@virtualads.com>
2017-08-29 12:09:15 +0200

    code cleanup

--- BitcoinAndCryptocurrencyTechnologies/Assignment2/src/CompliantNode.java
+++ BitcoinAndCryptocurrencyTechnologies/Assignment2/src/CompliantNode.java
@@ -9,7 +9,6 @@ public class CompliantNode implements Node {
     private HashSet<Transaction> pendingTxs;
     private HashSet<Transaction> incomingTxs;
     private HashMap< Integer, HashSet<Transaction> > incomingTxsFromAllNodes;
-    //private ArrayList<Boolean> nodeFollowees;
     private boolean[] nodeFollowees;
     private int numOfRounds = 0;
 
@@ -18,16 +17,12 @@ public class CompliantNode implements Node {
         pendingTxs = new HashSet<Transaction>();
         incomingTxs = new HashSet<Transaction>();
         incomingTxsFromAllNodes = new HashMap< Integer, HashSet<Transaction> >();
-        //nodeFollowees = new ArrayList<Boolean>();
         numOfRounds = numRounds+1;
     }
 
     public void setFollowees(boolean[] followees) {
         // IMPLEMENT THIS
         nodeFollowees = followees;
-        //for (int i=0; i<followees.length; i++) {
-        //    nodeFollowees.add( followees[i] ? Boolean.TRUE : Boolean.FALSE );
-        //}
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
@@ -48,7 +43,6 @@ public class CompliantNode implements Node {
         // IMPLEMENT THIS
         incomingTxs.clear();
         for (Candidate c : candidates) {
-            //if (!nodeFollowees.get(c.sender)) {
             if (!nodeFollowees[c.sender]) {
                 continue;
             }
@@ -59,7 +53,6 @@ public class CompliantNode implements Node {
             HashSet<Transaction> transactionsFromNode = incomingTxsFromAllNodes.get(c.sender);
             if (transactionsFromNode.contains(c.tx)) {
                 incomingTxsFromAllNodes.remove(c.sender);
-                //nodeFollowees.set(c.sender, Boolean.FALSE);
                 nodeFollowees[c.sender] = false;
                 continue;
             } else {
